9a6626a5c88815093edd4ef4fd30345bace7215a
Abhilash Thomas <abhilash2006@gmail.com>
2026-04-01 09:16:58 +0400

    CI/CD Check

--- FinTrackPortal.API/WeatherForecast.cs
+++ FinTrackPortal.API/WeatherForecast.cs
@@ -6,6 +6,8 @@ namespace FinTrackPortal.API
 
         public int TemperatureC { get; set; }
 
+        public string TempData => $"Temperature in Celsius: {TemperatureC}°C";
+
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
